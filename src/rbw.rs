1b8f09c3cda581b4a102c32e1157beade600e806
Hansen <samhansen.dev@gmail.com>
2025-07-26 21:02:19 +0300

    refactor: minor changes

--- src/rbw.rs
+++ src/rbw.rs
@@ -1,13 +1,33 @@
-use std::process::{Child, ChildStdout, Command, Stdio};
+use std::{
+    io::Read,
+    process::{ChildStdout, Command, Stdio},
+};
 
-pub fn spawn() -> (Child, ChildStdout) {
-    let mut rbw_child = Command::new("rbw")
+fn process_list(mut stdout: ChildStdout) -> String {
+    let mut list_string = String::new();
+
+    // read `rbw list` stdout into a string
+    stdout
+        .read_to_string(&mut list_string)
+        .expect("Failed to read string");
+
+    list_string
+}
+
+pub fn spawn() -> String {
+    // spawn `rbw list`
+    let mut child = Command::new("rbw")
         .arg("list")
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to spawn rbw");
 
-    let rbw_stdout = rbw_child.stdout.take().expect("Failed to read rbw_output");
+    let stdout = child.stdout.take().expect("Failed to read rbw_output");
 
-    (rbw_child, rbw_stdout)
+    // pass `rbw list` stdout to process_list to get the list in a Vec<String>
+    let list = process_list(stdout);
+
+    let _ = child.wait().expect("Failed to wait on rbw");
+
+    list
 }
