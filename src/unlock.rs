966da600b929c0080209546bd9be7a5c4e04b529
Hansen <samhansen.dev@gmail.com>
2025-09-10 12:13:32 +0300

    feat: change assets path

--- src/unlock.rs
+++ src/unlock.rs
@@ -7,8 +7,17 @@ pub fn unlock() {
         .output()
         .expect("Failed to unlock rbw");
 
+    // send a notification when unable to unlock the rbw database
+
     if !unlock.status.success() {
-        println!("{}", String::from_utf8_lossy(&unlock.stderr));
+        let icon_path: String = format!("/usr/share/fuzzel-rbw/assets/logos/bitwarden.svg");
+
+        let unlock_error = String::from_utf8_lossy(&unlock.stderr).to_owned();
+
+        Command::new("notify-send")
+            .args(["-i", &icon_path, "Fuzzel RBW", &unlock_error])
+            .spawn()
+            .expect("Failed to execute notify send");
     } else {
         insert::main();
     };
