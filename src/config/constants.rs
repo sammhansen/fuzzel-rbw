cd5769c9a029ab407ba46fb7b1a72267d9c1a4d6
Hansen <samhansen.dev@gmail.com>
2025-09-17 14:06:26 +0300

    fix: use bitwarden from pixmaps instead of path

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -1,3 +1,3 @@
-pub const FRBW_ICON: &str = "/usr/share/fuzzel-rbw/assets/logos/bitwarden.svg";
+pub const FRBW_ICON_NAME: &str = "bitwarden";
 pub const FRBW_NAME: &str = "Fuzzel RBW";
 pub const CONFIG_DIR: &str = "~/.config/fuzzel-rbw/config.json";
